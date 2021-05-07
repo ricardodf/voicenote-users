ca6219863910f0f46000224ba38d8257069692d8
Ricardo Diaz <39528804+ricardodf@users.noreply.github.com>
2021-05-06 23:20:59 -0500

    MongoDB fix 1

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 
@@ -11,56 +10,58 @@ const bcrypt = require('bcryptjs');
 const saltRounds = 10;
 
 app.use(cors());
-app.use(bodyParser.urlencoded());
-app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var db;
 
 MongoClient.connect(process.env.MONGODB_CONNECTION_STR, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database');
-
-    const db = client.db('vn-users');
-    const usersCollection = db.collection('users');
-
-    app.get('/users/userByEmail/', (req, res) => {
-      const { email, password } = req.body;
-      
-      usersCollection.findOne({ email })
-        .then(userFound => {
-          if(userFound) {
-            bcrypt.compare(password, userFound.password, function(err, result) {
-              if(result) res.status(200).send(userFound);
-              else res.status(403).send({ msg: "Invalid credentials"})
-            });
-          }
-          else {
-            res.status(404).send({ msg: `User not found with email: ${email}`})
-          }
-        })
-        .catch(error => res.status(400).send(error))
-    })
-
-    app.post('/users/new', (req, res) => {
-      const { id, name, email, password } = req.body;
-
-      bcrypt.hash(password, saltRounds, (err, hash) => {
-        const newUser = {
-          id: id,
-          name: name,
-          email: email,
-          password: hash
-        }
-        usersCollection.insertOne(newUser)
-          .then(result => res.status(201).send(result))
-          .catch(error => res.status(400).send(error))
-      });
-    })
-
+    db = client.db('vn-users');
+    app.listen(process.env.PORT || 5000);
   })
   .catch(error => console.error(error))
+
+
+app.get('/users/userByEmail/', (req, res) => {
+  const usersCollection = db.collection('users');
+  const { email, password } = req.body;
+  
+  usersCollection.findOne({ email })
+    .then(userFound => {
+      if(userFound) {
+        bcrypt.compare(password, userFound.password, function(err, result) {
+          if(result) res.status(200).send(userFound);
+          else res.status(403).send({ msg: "Invalid credentials"})
+        });
+      }
+      else {
+        res.status(404).send({ msg: `User not found with email: ${email}`})
+      }
+    })
+    .catch(error => res.status(400).send(error))
+})
+
+app.post('/users/new', (req, res) => {
+  const usersCollection = db.collection('users');
+  const { id, name, email, password } = req.body;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    const newUser = {
+      id: id,
+      name: name,
+      email: email,
+      password: hash
+    }
+    usersCollection.insertOne(newUser)
+      .then(result => res.status(201).send(result))
+      .catch(error => res.status(400).send(error))
+  });
+})
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT || 5000);
-module.exports = {app, MongoClient};
+module.exports = app;
